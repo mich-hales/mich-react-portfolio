b7c2f64e264b52ef4c91b03ac9840a697c6a88f9
mich-hales <michellehales93@gmail.com>
2022-12-06 10:57:25 -0700

    navbar and pages setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Contact from './pages/Contact';
+import AboutMe from './pages/AboutMe';
+import Portfolio from './pages/Portfolio';
+import Resume from './pages/Resume';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    let component;
+    // eslint-disable-next-line default-case
+    switch(window.location.pathname) {
+        case '/':
+            component = <Home />
+            break;
+        case '/contact':
+            component = <Contact />
+            break;
+        case '/aboutme':
+            component = <AboutMe />
+            break;
+        case '/portfolio':
+            component = <Portfolio />
+            break;
+        case '/resume':
+            component = <Resume />
+    }
+    return (
+        <div>
+            <Navbar />
+            <div className='component'>
+                {component}
+            </div>
+        </div>
+    )
 }
 
 export default App;
+
+
+ 
+   
---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Navbar() {
+    return (
+        <nav>
+            <a href="/">Home</a>
+            <a href="/aboutme">About Me</a>
+            <a href="/portfolio">Portfolio</a>
+            <a href="/resume">Resume</a>
+            <a href="/contact">Contact</a>
+        </nav>
+    )
+}
