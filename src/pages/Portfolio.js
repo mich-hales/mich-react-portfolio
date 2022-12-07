2f0248ff3a182c4e51f1494b55fc93efd9186470
mich-hales <michellehales93@gmail.com>
2022-12-07 13:25:42 -0700

    imported images and correctly sourced them

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,4 +1,15 @@
 import React from 'react';
+import AdobePhoto from '../images/AdobeStock_266849226.jpeg'
+import QuickFlix from '../images/flix.jpeg';
+import Weather from '../images/weather.jpeg';
+import CodingQuiz from '../images/quiz.png';
+import TeamProfiles from '../images/team-generator.webp';
+import WorkDayScheduler from '../images/list.webp';
+import ECommerce from '../images/ecommerce-backend.webp';
+import TechBlog from '../images/tech-blog.jpeg';
+import Readme from '../images/readme.png';
+import Notetaker from '../images/note-taker.png';
+
 
 export default function aboutMe() {
     return (
@@ -8,52 +19,52 @@ export default function aboutMe() {
             <div className="portfolioLinks">
                 
                     <div className="overlay link-pink" id="image-grid-col-row">
-                        <a href="https://github.com/mich-hales/FitCommit" target="_blank"><img className="link-images" src="./Assets/images/AdobeStock_266849226.jpeg" alt="two gym buddies motivating eachother"/></a> 
+                        <a href="https://github.com/mich-hales/FitCommit" target="_blank"><img className="link-images" src={AdobePhoto} alt="two gym buddies motivating eachother"/></a> 
                         <h3 className="h3-overlay">FitCommit Project</h3>
                     </div>
 
                     <div className="overlay link-pink">
-                        <a href="https://github.com/mich-hales/QuickFlix" target="_blank"><img className="link-images" src="./Assets/images/flix.jpeg" alt="tv with the word flix in neon displaying on the screen"/></a>
+                        <a href="https://github.com/mich-hales/QuickFlix" target="_blank"><img className="link-images" src={QuickFlix} alt="tv with the word flix in neon displaying on the screen"/></a>
                         <h3  className="h3-overlay small-overlay">QuickFlix</h3>
                     </div>
 
                     <div className="overlay link-blue">
-                        <a href="https://github.com/mich-hales/weather-dashboard" target="_blank"><img className="link-images" src="./Assets/images/weather.jpeg" alt="cloudy weather with sun"/></a>
+                        <a href="https://github.com/mich-hales/weather-dashboard" target="_blank"><img className="link-images" src={Weather}alt="cloudy weather with sun"/></a>
                         <h3  className="h3-overlay small-overlay">Weather Dashboard</h3>
                     </div>
 
                     <div className="overlay link-pink">
-                        <a href="https://github.com/mich-hales/coding-quiz" target="_blank"><img className="link-images" src="./Assets/images/quiz.png" alt="the phrase quiz time in writing"/></a> 
+                        <a href="https://github.com/mich-hales/coding-quiz" target="_blank"><img className="link-images" src={CodingQuiz} alt="the phrase quiz time in writing"/></a> 
                         <h3 className="h3-overlay small-overlay">Coding Quiz</h3>
                     </div>
                     
                     <div className="overlay link-pink">
-                        <a href="https://github.com/mich-hales/team-profile-generator" target="_blank"><img className="link-images" src="./Assets/images/team-generator.webp" alt="three members of a team with profiles"/></a> 
+                        <a href="https://github.com/mich-hales/team-profile-generator" target="_blank"><img className="link-images" src={TeamProfiles} alt="three members of a team with profiles"/></a> 
                         <h3 className="h3-overlay small-overlay">Team Profile Generator</h3>
                     </div>
                  
                     <div className="overlay link-pink">
-                        <a href="https://github.com/mich-hales/work-day-scheduler" target="_blank"><img className="link-images" src="./Assets/images/list.webp" alt="paper with pencil"/></a>
+                        <a href="https://github.com/mich-hales/work-day-scheduler" target="_blank"><img className="link-images" src={WorkDayScheduler} alt="paper with pencil"/></a>
                         <h3  className="h3-overlay small-overlay">Work Day Scheduler</h3>
                     </div>
 
                     <div className="overlay link-pink">
-                        <a href="https://github.com/mich-hales/E-Commerce-Back-End" target="_blank"><img className="link-images" src="./Assets/images/ecommerce-backend.webp" alt="backend computer logic"/></a>
+                        <a href="https://github.com/mich-hales/E-Commerce-Back-End" target="_blank"><img className="link-images" src={ECommerce} alt="backend computer logic"/></a>
                         <h3  className="h3-overlay small-overlay">E-Commerce Back End</h3>
                     </div>
                     
                     <div className="overlay link-pink">
-                        <a href="https://github.com/mich-hales/Tech-Blog" target="_blank"><img className="link-images" src="./Assets/images/tech-blog.jpeg" alt="computer with cup of coffee on table"/></a>
+                        <a href="https://github.com/mich-hales/Tech-Blog" target="_blank"><img className="link-images" src={TechBlog} alt="computer with cup of coffee on table"/></a>
                         <h3  className="h3-overlay small-overlay">Tech Blog</h3>
                     </div>
 
                     <div className="overlay link-pink">
-                        <a href="https://github.com/mich-hales/readme-generator" target="_blank"><img className="link-images" src="./Assets/images/readme.png" alt="the words read me written in white and placed in the middle of a black circle"/></a>
+                        <a href="https://github.com/mich-hales/readme-generator" target="_blank"><img className="link-images" src={Readme} alt="the words read me written in white and placed in the middle of a black circle"/></a>
                         <h3  className="h3-overlay small-overlay">Readme Generator</h3>
                     </div>
 
                     <div className="overlay link-pink">
-                        <a href="https://github.com/mich-hales/note-taker" target="_blank"><img className="link-images" src="./Assets/images/note-taker.png" alt="someone taking notes with a paper and pencil"/></a>
+                        <a href="https://github.com/mich-hales/note-taker" target="_blank"><img className="link-images" src={Notetaker} alt="someone taking notes with a paper and pencil"/></a>
                         <h3  className="h3-overlay small-overlay">Note Taker</h3>
                     </div>
             </div>
@@ -66,7 +77,7 @@ export default function aboutMe() {
                 <p className="row-1-content item-1">HTML / CSS</p>
                 <p className="row-1-content item-2">JavaScript</p>
                 <p className="row-1-content item-3">MySQL</p>
-                <p className="row-1-content item-4">MongoDB</p>
+                <p className="row-1-content item-4">React</p>
             </div>
             <div className="row-2">
                 <p className="row-2-content item-1">Object Oriented Programming (OOP)</p>
